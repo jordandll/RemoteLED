5a45bb58286614bb44087e0fa4f470be345184c7
Jordan Driscoll <drisjor@protonmail.com>
2021-11-16 22:39:33 -0500

    Created directory, 'LED', for LED related scripts/modules.

---
+++ LED/client_on.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import socket as sock
+
+sock.setdefaulttimeout(2)
+
+# Control codes.
+
+LED_ON=b'\x01'
+LED_OFF=b'\x00'
+LED_CHECK=b'\x10'
+
+# Common error messages specific to sockets.
+err_msg_timeout='Connection Error:\tConnection timeout error occurred with server at \'{0}\' listening on port {1:d}.'
+
+# Create client socket, denoted as 's'.
+s = sock.socket()
+
+# Connect to server at 'localhost' listening on port 9001.
+HOST='127.0.0.1'
+PORT=9001
+ADDR=(HOST, PORT)
+try:
+    s.connect(ADDR)
+except sock.timeout:
+    print(err_msg_timeout.format(*ADDR))
+    exit(1)
+
+# Send 'LED on' command to the server and wait for server response that merely indicates that the command was received.
+# An error here simply implies a connectivity issue as opposed to an error with command itself.
+
+sent=s.send(LED_ON)
+print(f'Client sent {sent:d} bytes of data to \'s://{HOST}:{PORT:d}/\'.')
+
+try:
+    msg_r=s.recv(1024)
+except sock.timeout:
+    print(err_msg_timeout.format(*ADDR))
+    exit(1)
+
+print('Server received the command.')
+
+
+# Wait for follow up response from the server indicating success/error with the command itself.
+try:
+    msg_r=s.recv(1024)
+except sock.timeout:
+    print(err_msg_timeout.format(*ADDR))
+    exit(1)
+
+print('The server sent:  \'' + str(msg_r, 'utf-8') + '\'.')
+
+# Close and disconnect.
+s.close()
+
+
+
